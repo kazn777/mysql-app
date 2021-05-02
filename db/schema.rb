aef626f39124b62a51b814b11b455364498ce4a1
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-05-02 23:08:07 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_120057) do
+ActiveRecord::Schema.define(version: 2021_05_02_130447) do
 
   create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "users_id", null: false
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_04_30_120057) do
     t.string "city"
     t.string "hobby"
     t.string "LikeType"
+    t.integer "age"
     t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
